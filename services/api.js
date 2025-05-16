fbb46bd0330eab1ec7abb57f934c5434dbefbea0
gregshk1 <george.emanuel01@gmail.com>
2025-05-16 15:59:33 -0300

    Desafio concluído

--- services/api.js
+++ services/api.js
@@ -1,14 +1,5 @@
 import axios from 'axios';
 
-<<<<<<< Updated upstream
-export const api  = axios.create({
-    baseURL: 'https://nuareafrukmnjnaakplk.supabase.co/rest/v1',
-    headers: {
-        apikey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA",
-        authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im51YXJlYWZydWttbmpuYWFrcGxrIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NjAyMjc0MjEsImV4cCI6MTk3NTgwMzQyMX0.3qUEldVsmcr_yrpf8N1-qBGdHLB1QCy9nB6-nApVQIA"
-    }
-})
-=======
 export const api = axios.create({
   baseURL: `${process.env.NEXT_PUBLIC_SUPABASE_URL}/rest/v1/`,
   headers: {
@@ -16,4 +7,3 @@ export const api = axios.create({
     authorization: `Bearer ${process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY}`,
   },
 });
->>>>>>> Stashed changes
